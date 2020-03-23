67f8efe35d0f874b9285b739c2586e5f4fc28454
stefan_adrian <39777353+games4life71@users.noreply.github.com>
2020-03-23 15:25:45 +0200

    Add files via upload
    Need to check the diagonal win  bug problem 
    V1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblRestart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(52, 30);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(749, 503);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Click += new System.EventHandler(this.Click_Board);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblRestart
+            // 
+            this.lblRestart.AutoSize = true;
+            this.lblRestart.Location = new System.Drawing.Point(49, 9);
+            this.lblRestart.Name = "lblRestart";
+            this.lblRestart.Size = new System.Drawing.Size(73, 17);
+            this.lblRestart.TabIndex = 2;
+            this.lblRestart.Text = "RESTART";
+            this.lblRestart.Click += new System.EventHandler(this.lblRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 564);
+            this.Controls.Add(this.lblRestart);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblRestart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+
+        private bool PlayerTurn = true;
+        //the virtual table so we can check if the game is won 
+        private char[,] Board = new char[3, 3]; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+         
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //create the buttons  
+            for(int i = 0; i<tableLayoutPanel1.RowCount;i++)
+                for(int j =  0; j<tableLayoutPanel1.ColumnCount;j++)
+                {
+                    Button button = new Button ();
+                    button.Dock = DockStyle.Fill;
+      
+                    button.FlatStyle = FlatStyle.Popup;
+                    button.ForeColor = Color.Red;
+                    button.FlatAppearance.BorderSize = 0;
+                    button.Margin = new Padding(0);
+                    button.Tag = new Tuple<int, int>(i, j); //position of the button 
+                    //add an event handler  to every button pressed 
+                    button.Click += Click_Board;
+                    tableLayoutPanel1.Controls.Add(button);
+
+                }
+
+        }
+
+     
+
+        private void Click_Board(object s  , EventArgs e )
+        {
+            //when the user presses a button 
+            if (!(s is Button)) return; 
+
+             else
+            {
+                //cast the sender into a button 
+                Button button = (Button)s;
+
+                if (PlayerTurn)
+                {
+
+                    TableLayoutPanelCellPosition pos = new TableLayoutPanelCellPosition();
+                    pos = tableLayoutPanel1.GetPositionFromControl(button);
+
+                    Board[pos.Row, pos.Column] = 'X';
+
+                    button.Text = "X";
+                    button.Font = new Font(button.Font.FontFamily,60, FontStyle.Bold);
+                    button.ForeColor = Color.Red;
+                    button.Enabled = false;
+                    PlayerTurn = false;
+
+                    //check if the move is win 
+
+                    if (CheckWin(Board, 'X', pos))
+                    {
+                        MessageBox.Show("X wins ! ", "WIN", MessageBoxButtons.OK);
+                        DialogResult dg = MessageBox.Show("Do you wish to restart ? ", "Restart", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dg == DialogResult.Yes)
+                        {
+                            RestartGame(Board);
+                        }
+                    }
+                  else  if (CheckTie(Board))
+                    {
+                        MessageBox.Show("Game is tie ", "Tie", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        DialogResult dg = MessageBox.Show("Do you wish to restart ? ", "Restart", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dg == DialogResult.Yes)
+                        {
+                            RestartGame(Board);
+                        }
+                    }
+                   
+                } 
+                else
+                {
+                    //the player2 turn  is 
+
+                    TableLayoutPanelCellPosition pos = new TableLayoutPanelCellPosition();
+                    pos = tableLayoutPanel1.GetPositionFromControl(button);
+
+                    
+                    Board[pos.Row, pos.Column] = 'O';
+                    button.Text = "O";
+                    button.ForeColor = Color.DarkBlue;
+                    button.Font = new Font(button.Font.FontFamily, 60,FontStyle.Bold);
+                    button.Enabled = false;
+                    PlayerTurn = true;
+                        
+
+                    if (CheckWin(Board, 'O', pos))
+                    {
+                        MessageBox.Show("O  wins ! ", "WIN", MessageBoxButtons.OK);
+                        DialogResult dg = MessageBox.Show("Do you wish to restart ? ", "Restart", MessageBoxButtons.YesNo , MessageBoxIcon.Question);
+                        if (dg == DialogResult.Yes)
+                        {
+                            RestartGame(Board);
+                        }
+                    }
+                   else  if (CheckTie(Board))
+                    {
+                        MessageBox.Show("Game is tie ", "Tie", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        DialogResult dg = MessageBox.Show("Do you wish to restart ? ", "Restart", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dg == DialogResult.Yes)
+                        {
+                            RestartGame(Board);
+                        }
+                    }
+
+                
+
+                }
+
+
+
+            }
+
+        }
+
+           
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for(int i =  0; i<3; i++)
+
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    Console.Write(Board[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+        /// <summary>
+        /// A method that checks the state of the game 1 - p1 wins , 2 -p2 wins  , 3 -Tie 
+        /// </summary>
+        /// <param The virtual table ="Board"></param>
+        /// <returns></returns>
+        private bool  CheckWin(char[,] Board, char player , TableLayoutPanelCellPosition pos )
+        {
+
+            bool isWin = false;
+
+            //only ckeck the column , row or diagonal
+            int x_Coord = pos.Row;
+            int y_Coord = pos.Column;
+
+            bool isWinDiag = true;
+            bool isWinRow = true;
+            bool isWinColumn = true;
+            bool isWinANTI = true; 
+
+            //check the row 
+            for (int i  = 0; i<3;i++)
+            { 
+                if(x_Coord == y_Coord)
+                {
+                    if (Board[i, i] != player) isWinDiag = false;
+                }
+               if (Board[x_Coord, i] != player) { isWinRow = false; break; }
+ 
+            }
+           
+            //check win in column 
+            for(int i = 0; i<3;i++)
+            {
+                if(Board[i, y_Coord] != player) { isWinColumn = false; break; }
+            }
+
+            //check win in anti-diagonal 
+            for(int i = 0; i < 3; i++)
+            {
+                int j = 2 - i; 
+                if(Board[i,j] != player)
+                {
+                    isWinANTI = false;
+                    break;
+                }
+
+            }
+
+            if (x_Coord == y_Coord && isWinDiag == true)
+            {
+                Console.WriteLine("win by diag"); return true;
+            }
+            else if (x_Coord + y_Coord + 1 == 3 && isWinANTI == true)
+            {
+                Console.WriteLine("win by antidiag"); return true;
+            }
+            else if (isWinColumn == true)
+            {
+                Console.WriteLine("win by col"); return true;
+            }
+            else if (isWinRow == true)
+            {
+                Console.WriteLine("row"); return true;
+            }
+            else return false; 
+           
+            return false;
+
+            ;
+        }
+      
+        
+        private void RestartGame(char [,] board )
+        {
+
+            for(int i = 0; i<tableLayoutPanel1.RowCount;i++)
+             for(int j = 0; j<tableLayoutPanel1.ColumnCount;j++)
+                {
+                    Button button = (Button)tableLayoutPanel1.GetControlFromPosition(i, j);
+                    //reset every button  of panel 
+                    button.Text = " ";
+                    button.Enabled = true; 
+                    Board[i, j] = default(char);
+                    
+                }
+            PlayerTurn = true; 
+        }
+
+        private bool CheckTie(char[,] board )
+        {
+
+
+            for (int i = 0; i < 3; i++)
+
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == default(char)) return false; 
+
+                }
+
+            }
+
+            return true;
+        }
+
+        private void lblRestart_Click(object sender, EventArgs e)
+        {
+            RestartGame(Board);
+        }
+    }
+
+
+
+    
+
+
+}
